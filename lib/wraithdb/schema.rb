6e31d1d3f6ba9191a187c34bd64524ab181ab70c
Nathan Baxter <nathan.baxter@airbnb.com>
2012-09-25 18:04:15 -0700

    Validate that the schema.rb looks reasonably correct before we eval it.

--- lib/wraithdb/schema.rb
+++ lib/wraithdb/schema.rb
@@ -1,5 +1,8 @@
 module WraithDB
   class Schema < ActiveRecord::Schema
+    SCHEMA_REGEX = /^ActiveRecord::Schema.define\(:version => \d+\) do/
+    END_REGEX = /^end\s*\z/
+
     class << self
       def write(*args)
         # normally this would be noisy like a migration, this makes it quiet
@@ -22,8 +25,12 @@ module WraithDB
         @loaded = true
         file = ENV['SCHEMA'] || "#{Rails.root}/db/schema.rb"
         source = File.read(file)
-        source.sub!(/^ActiveRecord::Schema.define\(:version => \d+\) do/, "")
-        source.sub!(/^end\s*\z/, "")
+        if (source =~ SCHEMA_REGEX && source =~ END_REGEX)
+          source.sub!(SCHEMA_REGEX, "")
+          source.sub!(END_REGEX, "")
+        else
+          raise StandardError.new("Invalid format for #{file}.")
+        end
 
         instance_eval(source)
       end
