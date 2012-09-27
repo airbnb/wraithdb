1ed6561e9512570c7883219a29d21bbfca5ebe89
Nathan Baxter <nathan.baxter@airbnb.com>
2012-09-27 09:59:55 -0700

    Changed our approach to handling db-charmer relations. It was causing MySQL locks during parallel testing.

--- lib/gems/db-charmer.rb
+++ lib/gems/db-charmer.rb
@@ -1,23 +1,24 @@
 module ActiveRecord
   class Base
     class << self
-      # This clobbers the DB Charmer wrapping of relation and does the same thing except
-      # it leaves connection resolution to runtime rather than assigning an instance variable
-      def relation_with_db_charmer(*args, &block)
-        relation_without_db_charmer(*args, &block).tap do |rel|
-          begin
-            rel.db_charmer_connection = @connection
-          rescue StandardError => e
-            base = self
-            rel.define_singleton_method(:db_charmer_connection) do
-              base.connection
-            end
+
+      def relation_with_wraith_charmed(*args, &block)
+        begin
+          relation_without_wraith_charmed(*args, &block)
+        rescue StandardError => e
+          # This leaves connection resolution for db_charmer to runtime rather than assigning an
+          # instance variable
+          base = self
+          rel = relation_without_db_charmer
+          rel.define_singleton_method(:db_charmer_connection) do
+            base.connection
           end
           rel.db_charmer_enable_slaves = self.db_charmer_slaves.any?
           rel.db_charmer_connection_is_forced = !db_charmer_top_level_connection?
+          rel
         end
       end
-      alias relation relation_with_db_charmer
+      alias_method_chain :relation, :wraith_charmed
     end
   end
 end
